a58e2a6e26d1fee762de755e9b4903a08dde281e
nntan041299 <nntan041299@gmail.com>
2026-07-20 11:02:13 +0700

    Add flag to enable jobs

--- english-master-service/src/main/java/com/nntan041299/englishmasterservice/listening/job/ListeningPracticeGenerationJob.java
+++ english-master-service/src/main/java/com/nntan041299/englishmasterservice/listening/job/ListeningPracticeGenerationJob.java
@@ -3,6 +3,7 @@ package com.nntan041299.englishmasterservice.listening.job;
 import com.nntan041299.englishmasterservice.listening.service.ListeningPracticeGenerationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -13,8 +14,14 @@ public class ListeningPracticeGenerationJob {
 
     private final ListeningPracticeGenerationService listeningPracticeGenerationService;
 
+    @Value("${listening.practice.generation.enabled}")
+    private boolean enabled;
+
     @Scheduled(cron = "${listening.practice.generation.cron}")
     public void run() {
+        if (!enabled) {
+            return;
+        }
         log.info("listening_practice_generation_job started");
         listeningPracticeGenerationService.generate();
     }
